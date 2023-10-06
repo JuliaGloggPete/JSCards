92cc138390564517190180d0311c2a88df776d03
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-10-06 08:29:46 +0200

    finished code along and added score

--- cards.js
+++ cards.js
@@ -1,14 +1,23 @@
+
+
+
+const lowerButton = document.querySelector('#lower')
+const higherButton = document.querySelector('#higher')
+const equalButton = document.querySelector('#equal')
+const showScore = document.querySelector('#score')
+
 const deck = generateDeck();
 let activeCard = {};
 pickCard();
 showCard();
+let score = 0;
 
 console.log(deck)
 
 
 function generateDeck(){
 let deck = [];
-const suits =['&spades;','&hearts;','&clubs;', '&daims;'];
+const suits =['&spades;','&hearts;','&clubs;', '&diams;' ];
 for(let i =0; i < suits.length; i++){
 for(let j = 2; j < 15; j++){
 let card ={
@@ -50,6 +59,7 @@ const pickedCard = deck.splice(randomPosition,1);
 console.log(pickedCard)
 activeCard = pickedCard[0];
 
+
 }
 
 function showCard(){
@@ -65,7 +75,69 @@ function showCard(){
     </footer>
     </section>
 `
-    
+}
 
+
+lowerButton.addEventListener('click', () =>{
+    /*spara värdet
+    välj lägre lika eller hägre
+    slumpa ny kort
+    spara kortet */
+
+    const previosCard = activeCard;
+    pickCard();
+    showCard();
+
+    console.log(previosCard)
+    console.log(activeCard)
+
+    if (activeCard.value < previosCard.value){
+        score++
+        showScore.innerText = 'Your score is ' + score;
+        console.log(score)
+    } else{
+        
+    }
+
+
+})
+higherButton.addEventListener('click', () =>{
+  
+
+    const previosCard = activeCard;
+    pickCard();
+    showCard();
+
+    console.log(previosCard)
+    console.log(activeCard)
+
+if (activeCard.value > previosCard.value){
+    score++;
+    showScore.innerHTML = 'Your score is ' + score;
+    console.log(score)
+} else{
 
 }
+
+})
+equalButton.addEventListener('click', () =>{
+   
+
+    const previosCard = activeCard;
+    pickCard();
+    showCard();
+
+    console.log(previosCard)
+    console.log(activeCard)
+
+    if (activeCard.value ===  previosCard.value){
+        let score = score++
+        showScore.innerText = 'Your score is ' + score;
+        console.log(score)
+
+    } else{
+        
+    }
+
+
+})
